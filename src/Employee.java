22015c1e14a8ef947dfaeb6e256b88152945e202
rahulrrsm <rahulrrsm@gmail.com>
2021-01-19 21:18:20 +0530

    done

--- src/Employee.java
+++ src/Employee.java
@@ -3,18 +3,6 @@
 public class Employee {
 	
 	public void empId() {
-		System.out.println("Employee id is :123");
-
+		System.out.println("1");
 	}
-	
-	
-	
-	
-	public static void main(String[] args) {
-		
-		Employee e=new Employee();
-		e.empId();
-	
-	}
-
 }
