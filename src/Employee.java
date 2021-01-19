bb0d64d23ca0ed037b17d7b91ac4c10e273411d2
rahulrrsm <rahulrrsm@gmail.com>
2021-01-19 12:34:37 +0530

    done

---
+++ src/Employee.java
@@ -0,0 +1,24 @@
+
+
+public class Employee {
+	
+	public void empId() {
+		System.out.println("Employee id is :123");
+
+	}
+	
+	public void empName() {
+		System.out.println("Employee name is :Rahul");
+
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Employee e=new Employee();
+		e.empId();
+		e.empName();
+	}
+
+}
